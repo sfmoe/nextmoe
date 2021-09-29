1c316ad6a4272e05045e230e8a48c6078dcec2b1
Moe Martinez <deadfocus@gmail.com>
2021-09-28 19:13:58 -0500

    styling updates and contact form addition

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import contentfulApi from "@utils/ContentfulApi";
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS, INLINES } from "@contentful/rich-text-types";
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 
 
 function renderOption(richTextBodyField){
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,32 @@
+import { useEffect } from 'react'
 
+export default function Contact(){
+    useEffect(() => {
+        document.querySelector("#__next").className = "contact-page";    
+      }, []);
 
-const Contact = ()=>{
-    return <h1>Contact</h1>
+    return (
+<div class="contact-form">
+    <form name="form1" id="ff" method="post" action="https://formspree.io/f/mgerdqnr">
+
+    <label>
+        <span>Name*:</span>
+        <input placeholder="Please enter your name" name="name" id="name" required="" type="text" />
+    </label>
+
+    <label>
+        <span>Email*:</span>
+        <input placeholder="youremail@email.com" name="email" id="email" required="" type="email" />
+    </label>
+
+    <label>
+        <span>Message*</span>
+        <textarea id="message" name="message" required=""></textarea>
+    </label>
+
+        <input class="sendButton" name="Submit" value="Send" type="submit" />
+    </form>
+    <div class="form-status">&nbsp;</div>
+</div>
+    );
 };
-
-export default Contact;
