6de9f9f287ca3085d997cfd52e50016e645d1bcc
Moe Martinez <deadfocus@gmail.com>
2021-09-30 20:27:23 -0500

    hopefully fixing a build issue where vercel couldnt find Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,19 +3,19 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Script from 'next/script';               
 
-class Layout extends React.Component {
-    render() {
+const Layout = (props) => {
+  
         return(
         <>
         <Header>
         <title>Moe Martinez | Photographer - Web Developer</title>   
         </Header>
         <Script src="https://use.fontawesome.com/2fa843e17f.js"></Script>
-        {this.props.children}
+        {props.children}
         <Footer></Footer>
         </>
         )
-    }
+
 }
 
 
