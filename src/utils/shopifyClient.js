af4ba64ad5462f938c1c3aaa7d618779778eb186
Moe Martinez <deadfocus@gmail.com>
2022-08-20 15:57:45 -0500

    remove old code

--- src/utils/shopifyClient.js
+++
@@ -1,5 +0,0 @@
-import Client from "shopify-buy";
-export const client = Client.buildClient({
-  storefrontAccessToken:     process.env.SHOPIFY_STORE_FRONT_ACCESS_TOKEN,
-  domain: process.env.SHOPIFY_STORE_DOMAIN
-});
