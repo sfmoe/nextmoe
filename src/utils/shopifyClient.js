e5b114b2271cccc5a1b0f9baadc297e75affa242
Moe Martinez <deadfocus@gmail.com>
2022-08-20 15:53:51 -0500

    style changes to gallery

---
+++ src/utils/shopifyClient.js
@@ -0,0 +1,5 @@
+import Client from "shopify-buy";
+export const client = Client.buildClient({
+  storefrontAccessToken:     process.env.SHOPIFY_STORE_FRONT_ACCESS_TOKEN,
+  domain: process.env.SHOPIFY_STORE_DOMAIN
+});
