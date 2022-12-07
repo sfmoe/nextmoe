ea86dba851b0ace868eebf3756d8a0fcd2f36f07
Moe Martinez <deadfocus@gmail.com>
2022-12-06 19:44:02 -0600

    add instagram images

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,35 @@
 import contentfulApi from "@utils/ContentfulApi";
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS } from "@contentful/rich-text-types";
-import { useEffect } from 'react'
+import { cloneElement, useEffect } from 'react'
 
-
-export default function About({pageContent}){
+//IG_API_TOKEN
+export default function About(props){
     useEffect(() => {
         document.querySelector("#__next").className = "about-page";    
+        
       }, []);
 
+     const pageContent = props.pageContent;
+     const igData = props.igData.data;
 
     return(
         <div id="main" className="content">
             <section>
         {documentToReactComponents(pageContent.pageContent.json, renderOption(pageContent.pageContent))}
+            </section>
+            <section className="ig-posts">
+              <h3>Latest Instagram Posts</h3>
+              {igData.map(e=>{
+                return(
+                  <a href={e.permalink} key={e.id}>
+                  <div className="ig-post">
+                    <img src={e.media_url} alt={e.caption}/>
+                    <span>{e.caption}</span>
+                  </div>
+                  </a>
+                )
+              })}
             </section>
         </div>
         )
@@ -48,9 +64,14 @@ function renderOption(richTextBodyField){
   
 
 export const getStaticProps = async ()=> {
+    const iglatestURL = await fetch(`https://graph.instagram.com/me/media?fields=id,media_url,caption,permalink&access_token=${process.env.IG_API_TOKEN}&limit=2`)
+    const igData = await iglatestURL.json();
+
+      console.log(igData)
+
     const page = await contentfulApi.getPageContet("About"); 
     return {
-        props: {pageContent: page},
+        props: {pageContent: page, igData: igData},
         revalidate: 60,
     };
 }
