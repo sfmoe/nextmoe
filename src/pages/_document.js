e03bf6bc47402f0a16460bcf764637ca46e57f6c
Moe Martinez <deadfocus@gmail.com>
2023-07-28 10:30:49 -0500

    jsx close meta tag fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,14 +7,14 @@ class MyDcoument extends Document{
                 <Head>
                 <link rel="icon" href="/assets/icon/favicon-16x16.png"  type="image/png" />
                 <meta name="description" content="Chicago based photographer. Beauty, Portraits, Headshots, Editorials and Street Photography." />
-                <meta property="og:site_name" content="Moe Martinez, Photographer">
-                <meta property="og:title" content="I'm Moe Martinez and I take photos.">
-                <meta property="og:url" content="https://moemartinez.com">
-                <meta property="og:description" content="I am a Chicago photographer, I want to take your photo!">
-                <meta property="og:image" content="https://moemartinez.com/assets/websitecard.png">
-                <meta property="og:image:alt" content="I take photos.">
+                <meta property="og:site_name" content="Moe Martinez, Photographer" />
+                <meta property="og:title" content="I'm Moe Martinez and I take photos." />
+                <meta property="og:url" content="https://moemartinez.com" />
+                <meta property="og:description" content="I am a Chicago photographer, I want to take your photo!" />
+                <meta property="og:image" content="https://moemartinez.com/assets/websitecard.png" />
+                <meta property="og:image:alt" content="I take photos." />
 
-		        <meta name="author" content="Moe Martinez">
+		        <meta name="author" content="Moe Martinez" />
 
                 </Head>
                 <body>
