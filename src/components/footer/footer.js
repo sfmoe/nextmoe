7e747a1ea981cb8425212e0e4ef60d02a01fa611
Moe Martinez <deadfocus@gmail.com>
2023-10-04 12:44:48 -0500

    update li

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ export default function Footer(){
             <>
             <footer>
                 <div className="social">
-                    <a href="https://www.linkedin.com/in/moemartinez" target="_blank" rel="noreferrer"><i className="fa fa-linkedin"></i><span>Linkedin Moe Martinez</span></a>
+                    <a href="https://www.linkedin.com/in/sfmoe" target="_blank" rel="noreferrer"><i className="fa fa-linkedin"></i><span>Linkedin Moe Martinez</span></a>
                     <a href="https://instagram.com/sfmoe" target="_blank" rel="noreferrer"><i className="fa fa-instagram"></i><span>Instagram @sfmoe</span></a>
                     {/* <a href="https://twitter.com/sfmoe" target="_blank" rel="noreferrer"><i className="fa fa-twitter"></i><span>Twitter @sfmoe</span></a> */}
                     <a href="https://github.com/sfmoe" target="_blank" rel="noreferrer"><i className="fa fa-github"></i><span>Github SFMoe</span></a>
