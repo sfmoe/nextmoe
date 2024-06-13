3b0498d55d0322a96cd4ce59d38fe2af11df107b
Moe Martinez <deadfocus@gmail.com>
2024-06-13 10:03:14 -0500

    updating fa icon code

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,16 +1,25 @@
+
+import { ReactElement } from 'react'
+import { faLinkedin , faInstagram, faGithub, faMastodon } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function Footer(){
+
         return (
-            <>
+     
             <footer>
                 <div className="social">
-                    <a href="https://www.linkedin.com/in/sfmoe" target="_blank" rel="noreferrer"><i className="fa fa-linkedin"></i><span>Linkedin Moe Martinez</span></a>
-                    <a href="https://instagram.com/sfmoe" target="_blank" rel="noreferrer"><i className="fa fa-instagram"></i><span>Instagram @sfmoe</span></a>
-                    {/* <a href="https://twitter.com/sfmoe" target="_blank" rel="noreferrer"><i className="fa fa-twitter"></i><span>Twitter @sfmoe</span></a> */}
-                    <a href="https://github.com/sfmoe" target="_blank" rel="noreferrer"><i className="fa fa-github"></i><span>Github SFMoe</span></a>
-                    <a rel="me" href="https://mastodon.social/@sfmoe">Mastodon</a>
+                    <a href="https://www.linkedin.com/in/sfmoe" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} /><span>Linkedin Moe Martinez</span></a>
+                    <a href="https://instagram.com/sfmoe" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faInstagram} /><span>Instagram @sfmoe</span></a>
+                    <a href="https://github.com/sfmoe" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} /><span>Github SFMoe</span></a>
+                    <a rel="me" href="https://mastodon.social/@sfmoe">
+                    <FontAwesomeIcon icon={faMastodon} /><span>Mastodon</span></a>
                 </div>
             </footer>
-            </>
+           
         );
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,15 @@ import Header from "./header/header";
 import Footer from "./footer/footer";
 import Script from 'next/script';               
 
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
 const Layout = (props) => {
         return(
         <>
         <Header props={props.children.props}/> 
-        <Script src="https://use.fontawesome.com/2fa843e17f.js"></Script>
+        {/* <script src="https://kit.fontawesome.com/b4082de183.js" crossOrigin="anonymous"></script> */}
         {props.children}
         <Footer></Footer>
         </>
